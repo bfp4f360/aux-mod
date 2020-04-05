28ababc40db7feba591cdf8c275df0127d5cd285
Namenai <bfp4f360@gmail.com>
2020-04-05 03:30:36 -0400

    Added comments

--- bash/compileMod.bat
+++ bash/compileMod.bat
@@ -4,13 +4,18 @@ REM https://stackoverflow.com/questions/18462169/how-to-loop-through-array-in-ba
 
 set outputFolder=C:\Users\Namenai\Desktop\aux_test_local\addons
 set sourceFolder=C:\Users\Namenai\Documents\GitHub\aux-mod\src\addons
+
+REM Folder of the addonBuilder.exe, needed so that the script can change directory to it
 set AddonBuilderPath=C:\Program Files (x86)\Steam\steamapps\common\Arma 3 Tools\AddonBuilder
 set tempFolder=C:\Users\Namenai\AppData\Local\Temp
+
+REM Files to keep when building, like .hpp and shit
 set includeFile=C:\Users\Namenai\Documents\GitHub\aux-mod\bash\include.txt
 
 REM Sadly for the file below, the filepath can not have any spaces
 set keyFile=C:\Users\Namenai\Documents\GitHub\aux-mod\keys\RD501_Aux_Mod.biprivatekey
 
+REM List of all PBO's
 set Arr[0]=RD501_AARF
 set Arr[1]=RD501_Droids
 set Arr[2]=RD501_End
@@ -29,8 +34,13 @@ set Arr[14]=RD501_Vehicle_Weapons
 set Arr[15]=RD501_Weapons
 set Arr[16]=RD501_Zeus
 
-
+REM Change directory to the addonBuilder.exe
 CD /D %AddonBuilderPath%
+
+REM ---DO NOT EDIT BELOW THIS LINE---
+REM ---DO NOT EDIT BELOW THIS LINE---
+REM ---DO NOT EDIT BELOW THIS LINE---
+REM ---DO NOT EDIT BELOW THIS LINE---
 
 set "x=0"
 
